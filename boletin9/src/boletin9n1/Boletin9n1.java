e10a2b558737b1d8324d9215ec458a7ffd1e9b06
jvidalmoreira <jvidalmoreira@victoria20.danielcastelao.org>
2017-11-20 08:43:38 +0100

    commit 1

---
+++ boletin9/src/boletin9n1/Boletin9n1.java
@@ -0,0 +1,151 @@
+
+package boletin9n1;
+import java.util.Scanner;
+
+public class Boletin9n1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,c,d,e,f,g,h,i,j,k,pos=0,neg=0,cero=0;
+        
+       a=  sc.nextInt();
+       if(a>0){
+           pos =pos+1;
+           
+       }
+       if(a<0){
+               neg=neg+1;
+              
+           }
+            if(a==0){
+                       cero= cero+1;
+                       }
+        b = sc.nextInt();
+               if(b>0){
+           pos =pos+1;
+           
+       }
+       if(b<0){
+               neg=neg+1;
+              
+           }
+            if(b==0){
+                       cero= cero+1;
+                       }
+        c = sc.nextInt();
+               if(c>0){
+           pos =pos+1;
+           
+       }
+       if(c<0){
+               neg=neg+1;
+              
+           }
+            if(c==0){
+                       cero= cero+1;
+                       }
+        d = sc.nextInt();
+             if(d>0){
+           pos =pos+1;
+           
+       }
+       if(d<0){
+               neg=neg+1;
+              
+           }
+            if(d==0){
+                       cero= cero+1;
+                       }
+        e= sc.nextInt();
+             if(e>0){
+           pos =pos+1;
+           
+       }
+       if(e<0){
+               neg=neg+1;
+              
+           }
+            if(e==0){
+                       cero= cero+1;
+                       }
+        f= sc.nextInt();
+             if(f>0){
+           pos =pos+1;
+           
+       }
+       if(f<0){
+               neg=neg+1;
+              
+           }
+            if(f==0){
+                       cero= cero+1;
+                       }
+        g= sc.nextInt();
+             if(g>0){
+           pos =pos+1;
+           
+       }
+       if(g<0){
+               neg=neg+1;
+              
+           }
+            if(g==0){
+                       cero= cero+1;
+                       }
+        h= sc.nextInt();
+             if(h>0){
+           pos =pos+1;
+           
+       }
+       if(h<0){
+               neg=neg+1;
+              
+           }
+            if(h==0){
+                       cero= cero+1;
+                       }
+        i= sc.nextInt();
+             if(i>0){
+           pos =pos+1;
+           
+       }
+       if(i<0){
+               neg=neg+1;
+              
+           }
+            if(i==0){
+                       cero= cero+1;
+                       }
+        j= sc.nextInt();
+             if(j>0){
+           pos =pos+1;
+           
+       }
+       if(j<0){
+               neg=neg+1;
+              
+           }
+            if(j==0){
+                       cero= cero+1;
+                       }
+        k= sc.nextInt();
+             if(k>0){
+           pos =pos+1;
+           
+       }
+       if(k<0){
+               neg=neg+1;
+              
+           }
+            if(k==0){
+                       cero= cero+1;
+                       }
+        
+        
+        System.out.println("positivos = "+pos+" negativos = "+neg+" ceros= "+cero);
+        
+        
+        
+    }
+    
+}
