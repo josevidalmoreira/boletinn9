be257cd3d2ba2f23aaafed6ed485954d84a51d84
jvidalmoreira <jvidalmoreira@victoria20.danielcastelao.org>
2017-11-20 08:43:49 +0100

    commit 1

---
+++ boletin9n3/src/boletin9n3/Boletin9n3.java
@@ -0,0 +1,14 @@
+
+package boletin9n3;
+
+
+public class Boletin9n3 {
+
+    public static void main(String[] args) {
+        float a,b;
+    Rectangulo rec = new Rectangulo();
+  
+        System.out.println("el area del rectangulo es"+rec.Calculararea());
+    }
+    
+}
